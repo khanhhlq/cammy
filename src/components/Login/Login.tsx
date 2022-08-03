9a0a9288887e5dd53ca1523eb272d92248f1477b
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-03 23:57:00 +0700

    Bugs(*): autoPlay - video

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <div className="main">
+            <div className="login-header">
+                <p>asd</p>
+            </div>
+
+            <div className="login">
+                <p>asd</p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
